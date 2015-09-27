307f8be16ce17e29901b95e2bbd82987d38cd281
Nik Kinkel <nskinkel@iastate.edu>
2015-09-27 00:09:09 -0500

    print some debugging info in Main

--- src/main/java/me/dwtj/jsrcdiff/Main.java
+++ src/main/java/me/dwtj/jsrcdiff/Main.java
@@ -6,6 +6,7 @@ import org.antlr.grammarsv4.JavaLexer;
 import org.antlr.grammarsv4.JavaParser;
 import org.antlr.v4.runtime.*;
 
+import me.dwtj.jsrcdiff.FieldMap;
 import me.dwtj.jsrcdiff.Util.Pair;
 
 
@@ -19,6 +20,16 @@ public class Main
         
         Pair<FieldMap,MethodMap> mapsA = getMapsFromFile(args[0]);
         Pair<FieldMap,MethodMap> mapsB = getMapsFromFile(args[1]);
+
+        System.out.println("\nmapsA Field keys:\n");
+        for (FieldMap.Key k: mapsA.fst.keySet()) {
+            System.out.println(k);
+        }
+
+        System.out.println("\nmapsA Field Ids:\n");
+        for (FieldMap.Value v: mapsA.fst.values()) {
+            System.out.println(v.getId());
+        }
     }
     
     public static Pair<FieldMap,MethodMap> getMapsFromFile(String filePath) throws IOException
